4533952490b103ef075e775015d28da08ae987aa
work_global <work_global@shj.com>
2018-12-02 22:45:46 +0800

    resolve url problem

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -5,5 +5,5 @@ from apps.goods.views import IndexView,DetailView,ListView
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^goods/(?P<sku_id>\d+)$', DetailView.as_view(), name='detail'),#详情页
-    url(r'^list/(?P<type_id>\d+)$', ListView.as_view(), name='list')
+    url(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$', ListView.as_view(), name='list')
 ]
